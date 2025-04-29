fd98ca44f8c0edd2edeb23ab9310daf71bdf0390
erezaeia <elinarezaeian2003@gmail.com>
2025-04-29 18:14:24 -0400

    half login/sign

--- data/users.js
+++ data/users.js
@@ -1,9 +1,10 @@
 import {users} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 import validation from '../helper.js';
+import bcrypt from 'bcryptjs';
+const saltRounds = 16;
 
 let exportedMethods = {
-
   async getUserById(id) {
     id = validation.checkId(id);
     const userCollection = await users();
@@ -11,16 +12,23 @@ let exportedMethods = {
     if (!user) throw 'Error: User not found';
     return user;
   },
-  async addUser(firstName, lastName, email, gender, city, state, age, hashedPassword, balance) {
-    firstName = validation.checkString(firstName, 'First name');
-    lastName = validation.checkString(lastName, 'Last name');
-    email = validation.checkEmail(email, 'Email');
-    gender = validation.checkString(gender,'Gender');
-    city = validation.checkString(city,'City');
-    state = validation.checkString(state,'State');
-    age = validation.checkAge(age,'Age');
-    balance = validation.checkBalance(balance,'Balance');
+  async register(firstName, lastName, email, gender, city, state, age, password, balance) {
+    firstName = validation.checkFirstName(firstName);
+    lastName = validation.checkLastName(lastName);
+    email = validation.checkEmail(email);
+    gender = validation.checkString(gender);
+    city = validation.checkString(city);
+    state = validation.checkString(state);
+    age = validation.checkNumber(age);
+    balance = validation.checkAmount(balance);
+    password = validation.checkPassword(password);
+    const hashedPassword= await bcrypt.hash(password, saltRounds);
 
+    const usersCollection = await users();
+    const existingUser = await usersCollection.findOne({ email: email });
+    if (existingUser) {
+      throw `A user with the email '${email}' already exists.`;
+    }
     let newUser = {
       firstName: firstName,
       lastName: lastName,
@@ -29,26 +37,59 @@ let exportedMethods = {
       city: city,
       state : state,
       age: age,
-      hashedPassword: hashedPassword,
+      password: hashedPassword,
       categories: ['Groceries','Shopping','Restaurant','Transportation','Rent'],
       fixedExpenses: [],
       balance: balance
     };
-    const userCollection = await users();
-    const newInsertInformation = await userCollection.insertOne(newUser);
-    if (!newInsertInformation.insertedId) throw 'Insert failed!';
-    return await this.getUserById(newInsertInformation.insertedId.toString());
+    
+    const insertInfo = await usersCollection.insertOne(newUser);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+      throw 'Could not register user due to a database error.';
+    }
+    return { registrationCompleted: true };
+  },
+  async login(email,password) {
+
+    email = validation.checkEmail(email);
+    password = validation.checkPassword(password)
+    email=email.toLowerCase();
+
+    const usersCollection = await users();
+
+    const existingUser = await usersCollection.findOne({ email: email });
+    if (!existingUser) {
+      throw `Either the userId or password is invalid`;
+    }
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if(!passwordMatch){
+      throw `Either the userId or password is invalid`;
+    }
+    
+    return {
+      id: existingUser._id.toString(),
+      firstName: existingUser.firstName.trim(),
+      lastName: existingUser.lastName.trim(),
+      email: existingUser.email.trim(),
+      gender: existingUser.gender.trim(),
+      city: existingUser.city.trim(),
+      state: existingUser.state.trim(),
+      age: existingUser.age,
+      balance: existingUser.balance,
+      categories: existingUser.categories,
+      fixedExpenses: existingUser.fixedExpenses
+    };
   },
   async updateUserPut(id, firstName, lastName, email, gender, city, state, age, hashedPassword, balance) {
     id = validation.checkId(id);
-    firstName = validation.checkString(firstName, 'first name');
-    lastName = validation.checkString(lastName, 'last name');
-    email = validation.checkEmail(email, 'Email');
-    gender = validation.checkString(gender,'Gender');
-    city = validation.checkString(city,'City');
-    state = validation.checkString(state,'State');
-    age = validation.checkAge(age,'Age');
-    balance = validation.checkBalance(balance,'Balance');
+    firstName = validation.checkFirstName(firstName);
+    lastName = validation.checkLastName(lastName);
+    email = validation.checkEmail(email);
+    gender = validation.checkString(gender);
+    city = validation.checkString(city);
+    state = validation.checkString(state);
+    age = validation.checkNumber(age);
+    balance = validation.checkAmount(balance);
 
     const usersCollection = await users();
     const usersCol = await usersCollection.findOne({_id: new ObjectId(id)});
@@ -62,14 +103,13 @@ let exportedMethods = {
       city: city,
       state : state,
       age: age,
-      hashedPassword: hashedPassword,
+      password: hashedPassword,
       categories: usersCol.categories,
       fixedExpenses: usersCol.fixedExpenses,
       balance: balance
     };
 
-    const userCollection = await users();
-    const updateInfo = await userCollection.findOneAndUpdate(
+    const updateInfo = await usersCollection.findOneAndUpdate(
       {_id: new ObjectId(id)},
       {$set: userUpdateInfo},
       {returnDocument: 'after'}
@@ -85,7 +125,7 @@ let exportedMethods = {
   async addCategoryById(userID, newCategory) {
 
     userID = validation.checkId(userID);
-    newCategory = validation.checkString(newCategory, 'Category');
+    newCategory = validation.checkString(newCategory);
 
     const userCollection = await users();
     const user = await userCollection.findOne({_id: new ObjectId(userID)});
@@ -108,7 +148,7 @@ let exportedMethods = {
   async deleteCategoryById(userID, categoryToDelete) {
 
     userID = validation.checkId(userID);
-    categoryToDelete = validation.checkString(categoryToDelete, 'Category');
+    categoryToDelete = validation.checkString(categoryToDelete);
 
     const userCollection = await users();
     const user = await userCollection.findOne({_id: new ObjectId(userID)});
@@ -129,9 +169,9 @@ let exportedMethods = {
   },
   async addFixedExpensesById(userID, title, category, amount){
     userID = validation.checkId(userID);
-    title = validation.checkString(title, 'Title');
-    category = validation.checkString(category, 'Category');
-    amount = validation.checkBalance(amount, 'Amount'); 
+    title = validation.checkString(title);
+    category = validation.checkString(category);
+    amount = validation.checkBalance(amount); 
 
     const userCollection = await users();
     const user = await userCollection.findOne({_id: new ObjectId(userID)});
@@ -176,9 +216,9 @@ let exportedMethods = {
   async updateFixedExpenseById(userID, expenseID, title, category, amount) {
     userID = validation.checkId(userID);
     expenseID = validation.checkId(expenseID);
-    title = validation.checkString(title, 'Title');
-    category = validation.checkString(category, 'Category');
-    amount = validation.checkBalance(amount, 'Amount');
+    title = validation.checkString(title);
+    category = validation.checkString(category);
+    amount = validation.checkBalance(amount);
   
     const userCollection = await users();
   
@@ -202,7 +242,6 @@ let exportedMethods = {
     if (!updateResult.value) throw 'Error: Could not update the fixed expense';
     return updateResult.value;
   }
-  
 };
 
 export default exportedMethods;
